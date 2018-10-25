7bb8382b66aa4b98d80d17a25d56b9eda9e045e3
Abhiraj <mohan056@umn.edu>
2018-10-25 14:46:28 -0500

    Added tie checker in KNN

--- Homeworks/HW2/src/kNN.m
+++ Homeworks/HW2/src/kNN.m
@@ -5,12 +5,19 @@ function [classPrediction] = kNN(K, testFeatureVector, trainingDataFeatureMatrix
 % First finding the raw distance between the testfeature vector and all the
 % training samples
 distances = sum((trainingDataFeatureMatrix - testFeatureVector).^2, 2);
-[~, indices] = sort(distances);
+[sorted_distances, indices] = sort(distances);
 sortedDistanceClassLabels = trainingDataTargetVector(indices);
 nearestKPointsClassLabels = sortedDistanceClassLabels(1:K);
+nearestEvenPointsClassLabels = sortedDistanceClassLabels(1:K+1);
+even_distances = sorted_distances(K+1);
+
 allClassLabels = unique(trainingDataTargetVector);
 
-if sum(nearestKPointsClassLabels == allClassLabels(1)) > sum(nearestKPointsClassLabels == allClassLabels(2))
+if unique(even_distances) == sorted_distances(1)
+    if sum(nearestEvenPointsClassLabels == allClassLabels(1)) == sum(nearestEvenPointsClassLabels == allClassLabels(2))
+        classPrediction = -1;
+    end
+elseif sum(nearestKPointsClassLabels == allClassLabels(1)) > sum(nearestKPointsClassLabels == allClassLabels(2))
     classPrediction = allClassLabels(1);
 else
     classPrediction = allClassLabels(2);
