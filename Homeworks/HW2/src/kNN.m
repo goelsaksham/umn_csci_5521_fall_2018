7aca45a8f91b3f28683d1d87043a4dea8c5107dd
Abhiraj <mohan056@umn.edu>
2018-10-25 23:25:49 -0500

    Reformed tie if condition

--- Homeworks/HW2/src/kNN.m
+++ Homeworks/HW2/src/kNN.m
@@ -5,7 +5,7 @@ function [classPrediction] = kNN(K, testFeatureVector, trainingDataFeatureMatrix
 % First finding the raw distance between the testfeature vector and all the
 % training samples
 distances = sum((trainingDataFeatureMatrix - testFeatureVector).^2, 2);
-[~, indices] = sort(distances);
+[sortedDistance, indices] = sort(distances);
 sortedDistanceClassLabels = trainingDataTargetVector(indices);
 nearestKPointsClassLabels = sortedDistanceClassLabels(1:K);
 
@@ -13,7 +13,7 @@ allClassLabels = unique(trainingDataTargetVector);
 
 difference = abs(sum(nearestKPointsClassLabels == allClassLabels(1))- sum(nearestKPointsClassLabels == allClassLabels(2)));
 
-if (difference == 1) && (sortedDistanceClassLabels(K) == sortedDistanceClassLabels(K+1))
+if (difference == 1) && (sortedDistance(K) == sortedDistance(K+1)) && (sortedDistanceClassLabels(K) ~= sortedDistanceClassLabels(K+1))
         classPrediction = -1;
 else
     if sum(nearestKPointsClassLabels == allClassLabels(1)) > sum(nearestKPointsClassLabels == allClassLabels(2))
