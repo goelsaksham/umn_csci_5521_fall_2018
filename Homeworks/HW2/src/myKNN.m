076515d5a5cf705d60c93474da3a09aeb286b29e
Abhiraj <mohan056@umn.edu>
2018-10-26 18:29:04 -0500

    final changes

--- Homeworks/HW2/src/myKNN.m
+++ Homeworks/HW2/src/myKNN.m
@@ -2,12 +2,12 @@ function [classification] = myKNN(filename, l, k)
 % First load the training data
 [train_X, train_y] = get_training_data(filename);
 [test_X, test_y] = get_test_data(filename);
-Projection_X = vertcat(train_X, test_X);
-Projection_y = vertcat(train_y, test_y);
+all_Data_X = vertcat(train_X, test_X);
+all_Data_y = vertcat(train_y, test_y);
 
 % Finding the digits for which we want the features
-[eightDigitFeatureMatrix, eightVec] = get_digit_feature_matrix(Projection_X, Projection_y, 8);
-[nineDigitFeatureMatrix, nineVec] = get_digit_feature_matrix(Projection_X, Projection_y, 9);
+[eightDigitFeatureMatrix, eightVec] = get_digit_feature_matrix(all_Data_X, all_Data_y, 8);
+[nineDigitFeatureMatrix, nineVec] = get_digit_feature_matrix(all_Data_X, all_Data_y, 9);
 
 % Making a new feature matrix that contains data from only these three
 % digits
@@ -15,7 +15,7 @@ X = vertcat(eightDigitFeatureMatrix, nineDigitFeatureMatrix);
 y = vertcat(eightVec, nineVec);
 
 % Run the PCA algorithm on the input feature matrix for the training set
-[principal_components_system, score, explained_var] = pca(X);
+[principal_components_system, ~, ~] = pca(X);
 principal_components = principal_components_system(:, 1:l);
 
 % Find the projected points from the PCA
@@ -28,6 +28,6 @@ for row_num = 1:size(Projection, 1)
     % Running kNN algorithm on every sample in the projection matrix
     classification(row_num) = kNN(k, Projection(row_num, :), Projection, y);
 end
-
+disp(confusionmat(y, classification));
 end
 
