8532ee43e7661709df14c435e5da8428340fef0c
Saksham Goel <goelx029@umn.edu>
2018-12-03 20:20:18 -0600

    Adding changes

--- Homeworks/HW4/src/hw4_part3.m
+++ Homeworks/HW4/src/hw4_part3.m
@@ -16,7 +16,7 @@ train_nine_features = pixels((digit_label==9) & (flags < 4), :);
 train_nine_labels = digit_label((digit_label==9) & (flags < 4), :);
 all_training_features = vertcat(train_eight_features, train_nine_features);
 all_training_features = all_training_features - mean(all_training_features);
-all_training_features = all_training_features / 256;
+%all_training_features = all_training_features / 256;
 all_training_labels = vertcat(train_eight_labels, train_nine_labels);
 
 validation_eight_features = pixels((digit_label==8) & (flags == 4), :);
@@ -25,7 +25,7 @@ validation_nine_features = pixels((digit_label==9) & (flags == 4), :);
 validation_nine_labels = digit_label((digit_label==9) & (flags == 4), :);
 all_validation_features = vertcat(validation_eight_features, validation_nine_features);
 all_validation_features = all_validation_features - mean(all_validation_features);
-all_validation_features = all_validation_features / 256;
+%all_validation_features = all_validation_features / 256;
 all_validation_labels = vertcat(validation_eight_labels, validation_nine_labels);
 
 test_eight_features = pixels((digit_label==8) & (flags == 5), :);
@@ -34,7 +34,7 @@ test_nine_features = pixels((digit_label==9) & (flags == 5), :);
 test_nine_labels = digit_label((digit_label==9) & (flags == 5), :);
 all_test_features = vertcat(test_eight_features, test_nine_features);
 all_test_features = all_test_features - mean(all_test_features);
-all_test_features = all_test_features / 256;
+%all_test_features = all_test_features / 256;
 all_test_labels = vertcat(test_eight_labels, test_nine_labels);
 
 
@@ -68,62 +68,18 @@ test_y(1, all_test_labels' == 9) = -1;
 test_y(2, all_test_labels' == 9) = 1;
 
 
-% %% K = 18 Training
-% minimal_validation_error = inf;
-% rng('default');
-% for random_restart = 1:10
-%     k = 18;
-%     rate=0.01;
-%     V = randn(k, size(training_X, 1)+1);
-%     W = randn(2, k+1);
-%     numEpochs = 300;
-%     totalError = zeros(numEpochs, 1);
-%     for i = 1:numEpochs
-%         current_Error = 0;
-%         for sample_num = 1:size(training_X, 2)
-%             x = training_X(:, sample_num);
-%             t = training_y(:, sample_num);
-%             [dE_dV,dE_dW,E,z,y] = deltaNN(V,W,x,t);
-%             V = V - (rate * dE_dV);
-%             W = W - (rate * dE_dW);
-%             current_Error = current_Error + E;    
-%         end
-%         totalError(i) = current_Error;
-%     end
-%     figure(random_restart);
-%     plot(totalError);
-%     
-%     num_validation_Error = 0;
-%     for sample_num = 1:size(validation_X, 2)
-%         x = validation_X(:, sample_num);
-%         t = validation_y(:, sample_num);
-%         [~,~,~,z,~] = deltaNN(V,W,x,t);
-%         if ~is_correct_prediction(t, z)
-%             num_validation_Error = num_validation_Error + 1;
-%         end
-%     end
-%     validation_Error = num_validation_Error/size(validation_X, 2);
-%     disp(validation_Error);
-%     if validation_Error < minimal_validation_error
-%         minimal_validation_error = validation_Error;
-%     end
-% end
-% 
-% disp(minimal_validation_error);
-% disp(k);
-
-
-%% K = 9 Training (Best Possible K)
-rng('default');
+%% Different K Training
+k = 3;
+rate=0.01;
+numEpochs = 300;
 min_validation_error = inf;
-min_V = zeros(3, size(training_X, 1)+1);
-min_W = zeros(2, 4);
-for random_restart = 1:1
-    k = 10;
-    rate=0.01;
+random_restart_num = -1;
+min_V = zeros(k, size(training_X, 1)+1);
+min_W = zeros(2, k + 1);
+rng('default');
+for random_restart = 1:10
     V = randn(k, size(training_X, 1)+1);
     W = randn(2, k+1);
-    numEpochs = 300;
     totalError = zeros(numEpochs, 1);
     for i = 1:numEpochs
         current_Error = 0;
@@ -137,8 +93,8 @@ for random_restart = 1:1
         end
         totalError(i) = current_Error;
     end
-    figure(random_restart);
-    plot(totalError);
+    %figure(random_restart);
+    %plot(totalError);
     
     num_validation_Error = 0;
     for sample_num = 1:size(validation_X, 2)
@@ -154,10 +110,11 @@ for random_restart = 1:1
        min_validation_error = validation_Error;
        min_V = V;
        min_W = W;
+       random_restart_num = random_restart;
     end
-    % disp(validation_Error);
-    
+    disp(validation_Error);
 end
+fprintf('Best Random Start: %d, Validation Error: %f\n', random_restart_num, min_validation_error);
 num_Test_Error = 0;
 for sample_num = 1:size(test_X, 2)
     x = test_X(:, sample_num);
@@ -168,10 +125,4 @@ for sample_num = 1:size(test_X, 2)
     end
 end
 num_Test_Error = num_Test_Error/size(test_X, 2);
-    
-disp(num_Test_Error);
-%     if validation_Error < 0.1
-%         break;
-%     end
-% disp(validation_Error);
-% disp(k);
+fprintf('Test Error: %f\n', num_Test_Error);
